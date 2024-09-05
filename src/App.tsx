f06e12cb8a39a0297784c562e5a0e6d9bb648471
Pliskin <arieljanickcarvalho@gmail.com>
2024-09-05 16:31:52 +0100

    Installed and set up necessary assets (fonts, colors)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@
 function App() {
 
   return (
-    <h1 className='text-blue-600'> Hello world </h1>
+    <>
+      <h1 className="text-6xl font-Odor">  UBIHERE </h1>
+        <span className=""> Projects | Sign up | Suggest </span>
+      <div className="h-8 bg-LightgrayUbihere-0">
+        <span className="text-DarkgrayUbihere-0"> Text inbox test</span>
+      </div>
+    </>
+
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { // we name our custom font "tiempos"
+        Odor: ['Odor', 'serif'],
+        Lexend: ['Lexend', 'serif'],
+      },
+
+      colors: {
+        YellowUbihere: ["#FBE662"],
+        LightgrayUbihere: ["#F1EFEF"],
+        DarkgrayUbihere: ["#828282"]
+      }
+    },
   },
   plugins: [],
 }
