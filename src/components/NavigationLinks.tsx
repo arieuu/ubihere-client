048642d5722c0a2a6c1bca4a643c25c37d037b14
Pliskin <arieljanickcarvalho@gmail.com>
2024-09-10 19:03:44 +0100

    Showing appropriate links for when user is logged in or not

--- src/components/NavigationLinks.tsx
+++ src/components/NavigationLinks.tsx
@@ -1,22 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 
 
 function NavigationLinks() {
 
+
     const onClickLogout = () => {
         localStorage.removeItem("loginToken")
         // localStorage.setItem("loginToken", "")
+        
+        // We set a state so that the component reloads and the appropriate links show right away
+
+        setReloadLinks("Out")
     }
 
+    const loggedInToken = localStorage.getItem("loginToken");
+    const [reloadLinks, setReloadLinks] = useState("")
+
     return(
-        <span className="mx-auto text-[17px] mb-14"> 
-            <Link className="hover:underline" to={"/"}> Projects </Link> 
-            | <Link className="hover:underline" to={"/signup"}> Sign up </Link>
-            | <Link className="hover:underline" to={"/login"}> Login </Link>
-            | <Link className="hover:underline" to={"/createproject"}> Create project </Link>
-            | <Link className="hover:underline" to={"/"} onClick={onClickLogout}> Logout </Link>
-        </span>
+        <>
+        {
+            loggedInToken ?
+
+            // Navigation links for logged in user
+
+            <span className="mx-auto text-[17px] mb-14"> 
+                <Link className="hover:underline" to={"/"}> Projects </Link> 
+                | <Link className="hover:underline" to={"/createproject"}> Create project </Link>
+                | <Link className="hover:underline" to={"/"} onClick={onClickLogout}> Logout </Link>
+            </span>:
+
+            // Navigation links for logged out user
+
+            <span className="mx-auto text-[17px] mb-14"> 
+                <Link className="hover:underline" to={"/"}> Projects </Link> 
+                | <Link className="hover:underline" to={"/signup"}> Sign up </Link>
+                | <Link className="hover:underline" to={"/login"}> Login </Link>
+            </span>
+        }
+        </>
     )
 }
 
